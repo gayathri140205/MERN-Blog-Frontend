880aee6675d55e53013bffce3b6434ab6de34e81
gayathri140205 <gayathri140205@gmail>
2024-11-11 14:45:09 +0530

    updated port

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Bind to all network interfaces
+    port: 5173,       // You can customize this port or use the default one
+  },
 });
-
